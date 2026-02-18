8dbf8452614c71a0814ed54e61643b040f9b96e7
Martin <168760158+martin0pl@users.noreply.github.com>
2026-02-18 07:38:47 +0100

    Pouvoir finir une tâche

--- src/task.rs
+++ src/task.rs
@@ -1,10 +1,12 @@
 pub struct Task {
+    done: bool,
     title: String
 }
 
 impl Task {
     pub fn new_t(title: String) -> Task {
         Self {
+            done: false,
             title : title,
         }
     }
