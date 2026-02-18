e91aa5e9f575b4f57225ad04734bd7626cbe4808
Martin <168760158+martin0pl@users.noreply.github.com>
2026-02-18 18:00:14 +0100

    Gestion du cas où on lance juste le script

--- src/main.rs
+++ src/main.rs
@@ -28,21 +28,28 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
 
-    // On ne prend pas le premier argument qui est le nom du programme
-    let args: Vec<String> = args[1..].to_vec();
-
-    // Gestion des commandes
-    if args[0] == "list" {
+    // Vérification de si il y a des arguments
+    if args.len() == 1 {
         app.show_tasks();
     }
-    if args[0] == "new" && args[1] == "task" {
-        if args.len() == 3 {
-            app.add_task(Task::new_t(args[2].clone()));
-            println!("Tâche <{}> ajoutée !",args[2]);
+    else
+    {
+        // On ne prend pas le premier argument qui est le nom du programme
+        let args: Vec<String> = args[1..].to_vec();
+
+        // Gestion des commandes
+        if args[0] == "list" {
+            app.show_tasks();
         }
-    }
-    else {
-        println!("Commande non reconnue");
+        if args[0] == "new" && args[1] == "task" {
+            if args.len() == 3 {
+                app.add_task(Task::new_t(args[2].clone()));
+                println!("Tâche <{}> ajoutée !",args[2]);
+            }
+        }
+        else {
+            println!("Commande non reconnue");
+        }
     }
 
 }
