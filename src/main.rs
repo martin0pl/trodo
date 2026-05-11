52814c5fc5efb0c30c1d5ebc2ee15ae576b2cc25
Martin <168760158+martin0pl@users.noreply.github.com>
2026-05-11 19:49:28 +0200

    Delete trodo help

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use std::io;
 
 fn main() {
 
-    const VERSION:&str = "0.9.4";
+    const VERSION:&str = "0.9.5";
 
     let home_dir = env::var("HOME").expect("Impossible to reach HOME directory");
     let save_file = format!("{}/.trodo-save.json", home_dir);
@@ -63,40 +63,6 @@ fn main() {
         // trodo tomorrow
         else if args.len() == 1 && args[0] == "tomorrow" {
             app.show_tomorrow_tasks();
-        }
-        // trodo help
-        else if args.len() == 1 && args[0] == "help" {
-            println!("Trodo Commands:");
-
-            println!("General:");
-            println!("  trodo                                     : Show all tasks without project");
-            println!("  trodo info                                : Show all info");
-            println!("  trodo version                             : Show the current version");
-            println!("  trodo help                                : Show this help message\n");
-
-            println!("Task Display:");
-            println!("  trodo list                                : Show all tasks without project");
-            println!("  trodo list YYYY-MM-DD                     : Show tasks at the specify date");
-            println!("  trodo today                               : Show the today's tasks");
-            println!("  trodo tomorrow                            : Show the tomorrow's tasks");
-            println!("  trodo soon                                : Show the tasks in the next 7 days");
-            println!("  trodo late                                : Show late tasks\n");
-
-            println!("Task Creation:");
-            println!("  trodo new task \"task name\"                : Add a new task");
-            println!("  trodo new task \"task name\" \"YYYY-MM-DD\"   : Add a new task with due date");
-            println!("  trodo new task \"task name\" today          : Add a task to do today");
-            println!("  trodo new task \"task name\" tomorrow       : Add a task to do tomorrow\n");
-
-            println!("Task Management:");
-            println!("  trodo done task_num                       : Mark a task as done");
-            println!("  trodo undone task_num                     : Mark a task as undone");
-            println!("  trodo delay task_num YYYY-MM-DD           : Delay a task to a day\n");
-
-            println!("Deletion:");
-            println!("  trodo delete task_num                     : Delete the task");
-            println!("  trodo delete done                         : Delete all the done tasks");
-            println!("  trodo delete all                          : Delete all the tasks");
         }
         // trodo list YYYY-MM-DD
         else if args.len() == 2 && args[0] == "list" {
